d2ec72fa84002c1a8726781edcdd4be9919877bc
Sunil Challagundla <sunil.challagundla@vedicsys.com>
2018-12-06 05:30:49 -0500

    first commit

---
+++ includes/class-wp-snippets-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       #
+ * @since      1.0.0
+ *
+ * @package    Wp_Snippets
+ * @subpackage Wp_Snippets/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Snippets
+ * @subpackage Wp_Snippets/includes
+ * @author     Sunil <info@psd2web.in>
+ */
+class Wp_Snippets_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
